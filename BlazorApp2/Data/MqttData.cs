14c8f93c9a327a745645eba0d588101d27d2e149
unknown <karim.alshanticc@gmail.com>
2020-11-15 09:52:03 +0400

    initial

---
+++ BlazorApp2/Data/MqttData.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp2.Data
+{
+    public class MqttData
+    {
+        [Required]
+        public string serverUri { get; set; }
+
+        [Required]
+        public string clientId { get; set; }
+
+    }
+}
